6aec2a5a3f4fd8488c760bd3ac29aaa9b6c3a42f
agocan <578843228@qq.com>
2025-11-11 09:41:10 +0800

    添加jdk8

---
+++ debian/openjdk/8/build.sh
@@ -0,0 +1,22 @@
+
+
+app_name=openjdk
+version=8u462-slim-12.10
+
+proxy_arg="--build-arg https_proxy=192.168.88.84:1080"
+img1=ccr.ccs.tencentyun.com/hank997/${app_name}:${version}
+img2=ccr.ccs.tencentyun.com/hank997/${app_name}:${version}-amd64
+img3=ccr.ccs.tencentyun.com/hank997/${app_name}:${version}-arm64
+
+docker build --platform linux/amd64 ${proxy_arg} -t ${img2} .
+docker build --platform linux/arm64 ${proxy_arg} -t ${img3} .
+
+docker push ${img2}
+docker push ${img3}
+
+docker rmi $img1 2> /dev/null
+docker manifest rm $img1
+docker manifest create --insecure --amend $img1 $img2 $img3
+docker manifest annotate $img1 $img2 --os linux --arch amd64
+docker manifest annotate $img1 $img3 --os linux --arch arm64 --variant v8
+docker manifest push --insecure $img1
